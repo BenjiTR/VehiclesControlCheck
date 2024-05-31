ad3b65fdd1882b4b3c5bd52135fafdb5bc42b9af
Benjamin <benjamintr@live.com>
2024-05-31 17:00:59 +0200

    Parte visual de HOME completa//Probado en android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,15 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonCol, IonImg, IonItem, IonInput, IonIcon, IonFooter, IonButton, IonCheckbox, IonLabel } from '@ionic/angular/standalone';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslationConfigService } from '../services/translation.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonCheckbox, IonButton, IonFooter, IonIcon, IonInput, IonItem, IonImg, IonCol, IonRow, IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule],
+  imports: [CommonModule, FormsModule, IonLabel, IonCheckbox, IonButton, IonFooter, IonIcon, IonInput, IonItem, IonImg, IonCol, IonRow, IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule],
 })
 export class HomePage implements OnInit{
+
+  public showPassword: boolean = false;
+  public email:string = "";
+  public Error:string = "";
+  public password:string = "";
+  public isLoading:boolean = false;
+  public rememberSession:boolean =false;
+
   constructor(
     private translate: TranslateService,
     private _translation:TranslationConfigService,
@@ -18,13 +29,27 @@ export class HomePage implements OnInit{
 
   ngOnInit(): void {
     this.translate.setDefaultLang(this._translation.getLanguage()) ;
-    console.log(this._translation.getDeviceLanguage())
 
   }
 
+  //CAMBIAR LENGUAJE
   changeLanguage(language:string){
+    this._translation.language = language
     this.translate.use(language)
   }
 
+  //TOOGLE PASSWORD
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+
+  sendValidation(){}
+
+  loginWithEmail(){}
+
+  restorePassword(){}
+
+  signInWithGoogle(){}
 
 }
