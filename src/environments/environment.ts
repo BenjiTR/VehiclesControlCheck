9673fe4fe9ee40208465ef157c03cda94a0ca325
Benjamin <benjamintr@live.com>
2024-06-03 11:39:25 +0200

    Configuración de FIREBASE variables de entorno en desarrollo y producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyBEUQrzovOog0EiV91wqLo004DyJukGqTY",
+    authDomain: "vehicles-control-ck.firebaseapp.com",
+    projectId: "vehicles-control-ck",
+    storageBucket: "vehicles-control-ck.appspot.com",
+    messagingSenderId: "329432960985",
+    appId: "1:329432960985:web:9d24d527e86415364b93c8",
+    measurementId: "G-GYSN7J9W02"
+  },
   production: false
 };
 
